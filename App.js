371ce1daaff0ec7dd09019be2fc4c1a8d893e10c
Karan sapra <88654249+CraveforSuccess@users.noreply.github.com>
2023-04-01 23:58:16 +0530

    Update App.js

--- App.js
+++ App.js
@@ -22,7 +22,9 @@ const Img_Schema = mongoose.model('Img_Schema', ImgSchema)
 
 
 const Storage = multer.diskStorage({
-  
+   destination: function (req, file, cb) {
+    cb(null, path.join(__dirname,'uploads');
+  },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
   }
